4d901dfa0b956f94f9fd2d104b856d1323a2f0ff
Marcel Moreau <marcelmoreau@gmail.com>
2026-02-14 11:43:06 -0500

    v

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from "url";
-
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -9,6 +9,14 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                test: resolve(__dirname, 'h1k9b4si5l.html'),
+            },
         },
     },
 });
